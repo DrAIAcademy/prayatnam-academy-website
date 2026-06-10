4ae7ac89c1b9d413b68e9bcbfdd9f17f72acf423
Dr AI Academy <draiacademy@gmail.com>
2026-06-10 12:39:15 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-const toggle = document.querySelector('.menu-toggle');
-const links = document.querySelector('.nav-links');
-if (toggle && links) {
-  toggle.addEventListener('click', () => links.classList.toggle('open'));
-  links.querySelectorAll('a').forEach(link => {
-    link.addEventListener('click', () => links.classList.remove('open'));
-  });
-}
